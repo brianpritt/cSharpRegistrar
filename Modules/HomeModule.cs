4833d209da8c0b4db829381c00a9dfe261ecd59e
Brian Pritt and Levi Bibo <brian+levi@>
2016-12-13 15:16:49 -0800

    add front end handler for student object

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -2,6 +2,7 @@ using Nancy;
 using System.Collections.Generic;
 using System;
 using Registrar.Objects;
+using Nancy.ViewEngines.Razor;
 
 namespace Registrar
 {
@@ -9,10 +10,41 @@ namespace Registrar
   {
     public HomeModule()
     {
-      Get["/"] = _ => {
-        return View["TEMPLATE.cshtml"];
+      Get["/"] = _ =>
+      {
+        return View["index.cshtml"];
       };
-
+      Get["/students"] = _ =>
+      {
+        List<Student> allStudents = Student.GetAll();
+        return View["students.cshtml", allStudents];
+      };
+      Get["/students/add_new"] = _ =>
+      {
+        List<Course> allCourses = Course.GetAll();
+        return View["new_student.cshtml", allCourses];
+      };
+      Post["/students/"] = _ =>
+      {
+        string name = Request.Form["student-name"];
+        DateTime enrollmentDate = (DateTime) Request.Form["date"];
+        Student newStudent = new Student(name, enrollmentDate);
+        newStudent.Save();
+        List<Student> allStudents = Student.GetAll();
+        return View["students.cshtml", allStudents];
+      };
+      Get["/students/{id}"] = parameters =>
+      {
+        Student newStudent = Student.Find(parameters.id);
+        return View["student.cshtml", newStudent];
+      };
+      Delete["/students/remove"] = _ =>
+      {
+        int studentId = int.Parse(Request.Form["student-id"]);
+        Student.DeleteStudent(studentId);
+        List<Student> allStudents = Student.GetAll();
+        return View["students.cshtml", allStudents];
+      };
     }
   }
 }
